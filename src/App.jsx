370c7158796bcadb201e07f0755131336a64464c
vet88 <playaibn@gmail.com>
2022-07-01 22:16:20 +0400

    Homepage design is done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,25 @@ function App() {
 
   return (
     <div className="App">
-     
+      <div className="home-div">
+
+        <div className="home-left">
+          <header className="home-header">
+            <img src="src/assets/chess.png" className="chess-icon" alt="chess icon" />
+            <span id="home-h"> Redberry Knight Cup </span>
+          </header>
+        </div>
+
+        <div className="home-right">
+          <div className="home-intro">
+            <p className="home-text">chess says <span className="home-text-span">a lot about</span><br></br> who we are </p>
+          </div>
+          <div className="home-btn">
+            <button> Get Started &nbsp; <img src="src/assets/btn-vector.png"></img></button>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
