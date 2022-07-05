f90227a1f0033dc23bc000f25f353700af23129f
vet88 <playaibn@gmail.com>
2022-07-05 17:17:26 +0400

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import './style.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
