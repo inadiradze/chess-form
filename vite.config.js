8adc2ee5932f67548c3ed92eb02c918632608e72
vet88 <playaibn@gmail.com>
2022-07-05 17:22:20 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Pages()]
+  plugins: [react(),
+    Pages({
+      dirs: [
+        { dir: 'src/pages', baseRoute: '' },
+      ],
+    }),
+  ],
 })
